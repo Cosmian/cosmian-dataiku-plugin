c5de2dccc61e4eead913617ed88e3e50c039203e
Bruno Grieder <bruno.grieder@cosmian.com>
2019-09-20 19:04:41 +0200

    attach to host network

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,9 @@
 
 export DSS_HOME=~/dss_home
 
-docker run -v ${DSS_HOME}:/home/dss/home -p 11000:11000 --rm --name dataiku_dss dataiku_dss
+docker run -v ${DSS_HOME}:/home/dss/home \
+  -p 11000:11000 \
+  --rm \
+  --net="host" \
+  --name dataiku_dss \
+  dataiku_dss
