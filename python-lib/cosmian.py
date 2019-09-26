42c540fa066c5262c5e09cbd4afe103178152052
Bruno Grieder <bruno.grieder@cosmian.com>
2019-09-26 16:27:01 +0200

    IP MPC via merge join endpoint

--- python-lib/cosmian.py
+++ python-lib/cosmian.py
@@ -79,6 +79,31 @@ def get_join_handle(session, server_url, views, join_type, join_key):
         raise ValueError("Failed establishing connection to the Cosmian Server at: %s" % server_url)
 
 
+# noinspection DuplicatedCode
+def run_ip_mpc(session, server_url, views):
+    headers = {
+        "Accept-Encoding": "gzip",
+        "Accept": "application/json"
+    }
+    params = {
+        'views': json.dumps(views),
+        "join_type": "recurring",  # FIXME this will have to go after the OVH demo
+    }
+    try:
+        r = session.get(
+            url="%smerge_join" % server_url,
+            params=params,
+            headers=headers
+        )
+        if r.status_code != 200:
+            raise ValueError("Cosmian Server:: Error querying join on  %s, status code: %s, reason :%s" % (
+                views, r.status_code, r.text))
+        resp = r.json()
+        return resp["handle"]
+    except requests.ConnectionError:
+        raise ValueError("Failed establishing connection to the Cosmian Server at: %s" % server_url)
+
+
 def get_schema(session, server_url, handle):
     headers = {
         "Accept-Encoding": "gzip",
@@ -138,3 +163,28 @@ def cosmian_type_2_dataiku_type(ct):
     if ct == "string" or ct == "hash" or ct == "blurred" or ct == "encrypted":
         return "string"
     return "object"
+
+
+def recover_datasets_info(datasets):
+    url = ''
+    views = []
+    # process the list of datasets
+    # collect the views which will be passed as parameter to the call
+    # views must all be from the same server
+    for dataset in datasets:
+        config = dataset.get_config()['params']['customConfig']
+        view = config['view_name']
+        # sorted = config['sorted']
+        server_url = config['server_url']
+        if not server_url.endswith("/"):
+            server_url += "/"
+        if url == '':
+            url = server_url
+        else:
+            if url != server_url:
+                raise ValueError("All datasets must be accessed from the same Cosmian server")
+        views.append(view)
+    return {
+        url: url,
+        views: views
+    }
